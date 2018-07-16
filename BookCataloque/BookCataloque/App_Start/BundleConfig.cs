30b80c26ed351a0d6949c0ae967176c2845d8468
MakarovaOlga <35731398+MakarovaOlgaA@users.noreply.github.com>
2018-07-17 01:24:18 +0300

    added knockout to search form

--- BookCataloque/BookCataloque/App_Start/BundleConfig.cs
+++ BookCataloque/BookCataloque/App_Start/BundleConfig.cs
@@ -8,8 +8,10 @@ namespace BookCataloque
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/base-bundle").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/knockout-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,15 +21,13 @@ namespace BookCataloque
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/book-grid").Include(
-                      "~/Scripts/Books/book-grid.js"));
+            bundles.Add(new ScriptBundle("~/bundles/book-search").Include(
+                      "~/Scripts/Books/book-grid.js",
+                      "~/Scripts/Books/book-filter.js"));
         }
     }
 }
---
+++ BookCataloque/BookCataloque/Scripts/Books/book-filter.js
@@ -0,0 +1,27 @@
+﻿var BookFilter = BookFilter || {};
+
+(function () {
+    var self = this;
+    var filterForm = "#filterForm";
+    var dateFormat = 'DD.MM.YYYY';
+
+    self.BookFilterVM = {
+        RatingLowerBound: ko.observable(),
+        RatingUpperBound: ko.observable(),
+        PublicationDateLowerBound: ko.observable(),
+        PublicationDateUpperBound: ko.observable(),
+        PagesLowerBound: ko.observable(),
+        PagesUpperBound: ko.observable(),
+        Title: ko.observable(),
+        IsValid: ko.observable(true)
+    }
+
+    self.Initialize = function () {
+        ko.applyBindings(self.BookFilterVM, $(filterForm)[0]);
+
+        $('.datepicker').datetimepicker({
+            viewMode: 'days',
+            format: dateFormat
+        });
+    }
+}).apply(BookFilter);
